6312a2632a9b735ddccf6af5358a5f3c39939dd4
Anwara Favour <favourchika02@gmail.com>
2023-06-29 02:30:31 +0100

    Completed

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const path = require('path')
+
+const app = express();
+
+// enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.use('/openai', require('./routes/openaiRoutes'))
+
+app.listen(port, ()=> console.log(`server started on port ${port}`))
